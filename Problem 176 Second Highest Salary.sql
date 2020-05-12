66c5bc5621a0e05064e3cbbfb0649b3f9bb734ca
ravishagupta <ravishagupta96@gmail.com>
2020-05-12 16:15:56 -0500

    Solved 5 SQL problems from LeetCode

---
+++ Problem 176 Second Highest Salary.sql
@@ -0,0 +1,34 @@
+/*
+LeetCode
+176. Second Highest Salary
+
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+*/
+
+SELECT MAX(SALARY) AS SecondHighestSalary
+FROM EMPLOYEE
+WHERE SALARY NOT IN (SELECT MAX(SALARY) FROM EMPLOYEE);
+------------------------------------------------------------
+
+SELECT (SELECT DISTINCT SALARY 
+FROM EMPLOYEE
+ORDER BY SALARY DESC
+LIMIT 1 OFFSET 1) AS SecondHighestSalary
+
---
+++ Problem 183 Customers Who Never Order.sql
@@ -0,0 +1,48 @@
+/*
+LeetCode
+Problem: 183. Customers Who Never Order
+
+
+Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+Table: Customers.
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+
+Table: Orders.
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+
+Using the above tables as example, return the following:
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+*/
+
+SELECT NAME AS Customers
+FROM CUSTOMERS 
+left join ORDERS 
+ON CUSTOMERS.ID = ORDERS.CUSTOMERID
+WHERE ORDERS.CUSTOMERID is null
+
+-------------------------------------
+SELECT NAME AS Customers FROM CUSTOMERS
+WHERE CUSTOMERS.ID NOT IN (SELECT
+                          CUSTOMERID FROM ORDERS)
