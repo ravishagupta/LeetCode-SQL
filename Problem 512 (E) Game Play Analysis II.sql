29c22d410e99cc853e39b0e15e5d8adc9c507fd0
ravishagupta <ravishagupta96@gmail.com>
2020-05-22 17:58:57 -0500

    Next 6 SQL LeetCode Questions

---
+++ Problem 512 (E) Game Play Analysis II.sql
@@ -0,0 +1,58 @@
+/*
+LeetCode
+Problem: 512 Game Play Analysis II
+
+Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key of this table.
+This table shows the activity of players of some game.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on some day using some device.
+
+ 
+
+Write a SQL query that reports the device that is first logged in for each player.
+
+The query result format is in the following example:
+
+Activity table:
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-05-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+
+Result table:
++-----------+-----------+
+| player_id | device_id |
++-----------+-----------+
+| 1         | 2         |
+| 2         | 3         |
+| 3         | 1         |
++-----------+-----------+
+
+*/
+
+SELECT PLAYER_ID, DEVICE_ID
+FROM ACTIVITY 
+WHERE (PLAYER_ID, EVENT_DATE) IN (SELECT PLAYER_ID, MIN(EVENT_DATE)
+FROM ACTIVITY
+GROUP BY PLAYER_ID) 
+------------------------------------------
+SELECT A.PLAYER_ID, A.DEVICE_ID 
+FROM ACTIVITY AS A
+JOIN (SELECT PLAYER_ID, MIN(EVENT_DATE) AS MINDATE
+     FROM ACTIVITY
+     GROUP BY PLAYER_ID) AS R
+ON A.PLAYER_ID = R.PLAYER_ID AND A.EVENT_DATE = R.MINDATE
